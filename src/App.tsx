a68bfa35eb4b6f2ea2963d6b2808ee82340f5114
Kalydy Barnabás <barnabas.kalydy@gmail.com>
2023-06-20 19:56:43 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      {/* <Navbar /> */}
+      <Routes>
+        <Route path="/" element={"home"} />
+        <Route path="contact" element={"contact"} />
+        <Route path="*" element={"404 page here"} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
