e49141250380ce84c9ca79d033b295a2671b5027
Kalydy Barnabás <barnabas.kalydy@gmail.com>
2023-06-21 12:12:03 +0200

    Worked some on the info page

--- src/pages/InfoPage.tsx
+++ src/pages/InfoPage.tsx
@@ -1,8 +1,48 @@
+import { Typography } from "@mui/material";
+
 export default function InfoPage() {
   return (
-    <div>
-      Ezen az oldalon egy általános tájékoztató lesz, hogy hogyan fog kinézni a
-      közös munka
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        padding: "2rem",
+        backgroundColor: "#15ab1f",
+        gap: "3rem",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "1.5rem",
+        }}
+      >
+        <Typography style={{ fontSize: "1.4rem" }}>
+          Hogyan néz ki velem a közös munka?
+        </Typography>
+        <Typography>
+          A tanítási módszerem inkább egyfajta <strong>mentorálás</strong>, ahol
+          ingyenes forrásokat és saját tananyagokat javaslok Neked és a közös
+          órákon inkább az elakadásaidat nézzük át, mintsem hogy ott magyaráznám
+          ell nulláról a tananyagot. A legtöbb esetben
+          <strong> angol nyelvű </strong>tananyagokat fogok javasolni, mivel a
+          programozásnál az angol tudás is alapvető készség, így az egy jó
+          megszokás, ha már a tanulás közben is igyekszel az angol nyelvet
+          használni.
+        </Typography>
+        <Typography>
+          A közös munka megkezdése után el fogok neked küldeni egy (angol
+          nyelvű) összegzést a lépésekről, amik ahhoz szükségesek, hogy
+          megszerezd az első programozó állásodat. Ez a dokumentum végigkísér
+          majd a tanulásban és sokszor támaszként szolgálhat ha elakadtál, vagy
+          azon gondolkozol, hogy mi lehet a következő lépés a tanulásban.
+        </Typography>
+      </div>
+      <img
+        src="/infopage-cooperation.jpg"
+        style={{ maxWidth: "40%", borderRadius: "3%" }}
+      />
     </div>
   );
 }
--- src/sections/OffersSection.tsx
+++ src/sections/OffersSection.tsx
@@ -4,18 +4,9 @@ import {
   CardMedia,
   Typography,
   CardContent,
-  styled,
 } from "@mui/material";
 import { offers } from "../data";
 import { useNavigate } from "react-router-dom";
-
-const StyledContainerDiv = styled("div")({
-  display: "flex",
-  flexDirection: "row",
-  justifyContent: "space-around",
-  gap: "8%",
-  alignItems: "center",
-});
 
 export default function OffersSection() {
   const navigate = useNavigate();
@@ -25,7 +16,15 @@ export default function OffersSection() {
       <Typography sx={{ fontSize: "1.5rem" }}>
         Ha szeretnél együtt dolgozni velem, a következők szerint van lehetőséged
       </Typography>
-      <StyledContainerDiv>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-around",
+          gap: "8%",
+          alignItems: "center",
+        }}
+      >
         {offers.map((offer, index) => (
           <Card
             key={offer.name}
@@ -45,7 +44,7 @@ export default function OffersSection() {
             </CardActionArea>
           </Card>
         ))}
-      </StyledContainerDiv>
+      </div>
     </div>
   );
 }
